d56640ffd49e43bc77747ebed9b7f1d498fd8d3c
EranZ2025 <eranzehavi@gmail.com>
2025-07-06 19:58:45 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h1>Welcome to VocalMaster</h1>
+      <h1>Welcome to VocaMaster</h1>
       <p>This is your opera voice training app.</p>
       <p><a href="/learn">Learn</a></p>
       <p><a href="/practice">Go to Practice</a></p>
