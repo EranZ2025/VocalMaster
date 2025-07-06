24782bc6f05e5e0b47246a649e86a8ec7b9f8bfb
EranZ2025 <eranzehavi@gmail.com>
2025-07-06 20:02:05 +0200

    Update learn.js

--- pages/learn.js
+++ pages/learn.js
@@ -7,7 +7,7 @@ export default function Learn() {
         <li>IPA pronunciation guides</li>
         <li>Vocal styles and examples</li>
       </ul>
-      <a href="/">← Back to Home</a>
+      <p><a href="/">← Back to Home</a></p>
     </div>
   );
 }
