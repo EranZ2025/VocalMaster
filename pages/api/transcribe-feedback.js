d59a573a81b3bc5b77142e4725846b2522e48d65
EranZ2025 <eranzehavi@gmail.com>
2025-07-06 19:41:58 +0200

    Update transcribe-feedback.js

--- pages/api/transcribe-feedback.js
+++ pages/api/transcribe-feedback.js
@@ -1,8 +1,5 @@
-// /pages/api/transcribe-feedback.js
-
 export default function handler(req, res) {
   res.status(200).json({
     transcript: "This is a sample transcript of your singing.",
     feedback: "Your pitch is mostly accurate, but try to relax your jaw for smoother vowels.",
   });
-}
