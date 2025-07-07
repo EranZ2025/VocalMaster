924af7121452aba52b97dc432af4d7c3b8ac192a
EranZ2025 <eranzehavi@gmail.com>
2025-07-07 19:23:57 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 export default function Home() {
   return (
     <div style={{ padding: '20px', fontFamily: 'Arial' }}>
@@ -5,8 +7,21 @@ export default function Home() {
       <p>This app helps you learn opera singing and track your vocal progress.</p>
 
       <ul>
-        <li><a href="/learn">🎓 Go to Learn Section</a></li>
-        <li><a href="/practice">🎤 Go to Practice Section</a></li>
+        <li>
+          <Link href="/learn/anatomy">🫁 Vocal Anatomy</Link>
+        </li>
+        <li>
+          <Link href="/learn/ipa">💬 IPA</Link>
+        </li>
+        <li>
+          <Link href="/learn/styles">🎶 Vocal Styles</Link>
+        </li>
+        <li>
+          <Link href="/learn/masters">🎤 Masterclasses from Great Singers</Link>
+        </li>
+        <li>
+          <Link href="/practice">📝 Go to Practice Section</Link>
+        </li>
       </ul>
     </div>
   );
