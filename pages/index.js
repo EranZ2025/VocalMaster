6f682066032021f76fd084452e91752133ac158f
EranZ2025 <eranzehavi@gmail.com>
2025-07-07 23:01:16 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,17 @@
+import React from 'react';
 import Link from 'next/link';
 
-export default function Home() {
+export default function HomePage() {
   return (
-    <div style={{ padding: '2rem' }}>
+    <div>
       <h1>🎤 VocalMaster</h1>
       <p>Welcome to your vocal training hub. Choose a section below to begin:</p>
       <ul>
-        <li><Link href="/learn">Learning</Link></li>
-        <li><Link href="/exercises">Exercises</Link></li>
-        <li><Link href="/record">Record</Link></li>
-        <li><Link href="/progress">Progress</Link></li>
-        <li><Link href="/profile">Profile</Link></li>
+        <li>🎓 <Link href="/learn">Learning</Link></li>
+        <li>🏋️ <Link href="/exercises">Exercises</Link></li>
+        <li>🎙️ <Link href="/record">Record</Link></li>
+        <li>📈 <Link href="/progress">Progress</Link></li>
+        <li>👤 <Link href="/profile">Profile</Link></li>
       </ul>
     </div>
   );
